93d5665e8acebab5c64b78e1c909c95db5efc812
viren300 <viruchoapade300@gmail.com>
2023-01-24 14:40:18 +0530

    First Commit . . .

---
+++ eclipse-workspace/GitDemoProj/src/Recurssion_1.java
@@ -0,0 +1,41 @@
+
+public class Recurssion_1 {
+	static int[] arr = {2,4,36, 46, 100};
+public static void main(String[] args) {
+	
+	int value = SumOfDigit(arr.length-1);
+	System.out.println(value);
+	
+	int val_fact = Fact(5);
+	System.out.println(val_fact);
+	
+	int fib = Fib(7);
+	System.out.println(fib);
+	
+}
+
+	
+private static int Fib(int n) {
+	// TODO Auto-generated method stub
+	if(n==0 || n==1)
+	return n ;
+	return Fib(n-1)+Fib(n-2);
+}
+
+
+private static int Fact(int i) {
+	// TODO Auto-generated method stub
+	if(i==1)
+	return 1;
+	return i*Fact(i-1);
+	
+}
+
+
+private static int SumOfDigit(int n) {
+	// TODO Auto-generated method stub
+	if(n==0)
+	return arr[n];
+	return arr[n]+SumOfDigit(n-1);
+}
+}
